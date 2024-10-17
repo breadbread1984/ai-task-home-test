41f060619412644c3f27f75dce98ad8b98a18266
breadbread1984 <breadbread1984@163.com>
2024-10-17 11:18:54 +0800

    update

---
+++ src/models/cifar10_diffusion_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import wandb
+import torch
+import torch.nn as nn
+from pytorch_lightning import LightningModule
+
+class CIFAR10DiffusionModule(LightningModule):
+  def __init__(self,
+               model: nn.Module,
+               **kwargs
+  ):
+    super().__init__()
+    self.save_hyperparameters()
+
+    self.model = model
+    self.criterion = torch.nn.MSELoss()
+
+  def forward(self, x, t):
+    self.model(x, t)
+
+  def configure_optimizer(self):
+    return torch.optim.Adam(self.parameters(), lr = self.hparams.lr)
+
+  def training_step(self, batch, batch_idx):
+    log_dict, loss = self.step(batch, batch_idx, optimizer_idx)
+    self.log_dict({"/".join(("train", k)): v for k, v in log_dict.items()})
+    return loss
+
+  def validation_step(self, batch, batch_idx):
+    log_dict, loss = self.step(batch, batch_idx, optimizer_idx)
+    self.log_dict({"/".join(("train", k)): v for k, v in log_dict.items()})
+    return None
+
+  def test_step(self, batch, batch_idx):
+    log_dict, loss = self.step(batch, batch_idx, optimizer_idx)
+    self.log_dict({"/".join(("train", k)): v for k, v in log_dict.items()})
+    return None
+
+  def step(self, batch, batch_idx):
+    log_dict = {}
+    x, _ = batch # train non conditional diffusion, use no class
+    t = torch.randint(0, 1000, (x.size(0),), device = self.device).long()
+    noise = torch.randn_like(x)
+    x_noisy = x + noise * t[:, None, None, None] # forward process
+    output = self.model(x_noisy, t)
+    loss = self.criterion(output, noise)
+    log_dict['loss'] = loss
+    return log_dict, loss
+
+  def on_epoch_end(self):
+    self.model.eval()
+    with torch.no_grad():
+      image = self.model.sample()
+    for logger in self.trainer.logger:
+      if type(logger).__name__ == "WandbLogger":
+        logger.experiment.log({"gen_imgs": image})
--- src/models/modules/diffusion.py
+++ src/models/modules/diffusion.py
@@ -7,7 +7,7 @@ import numpy as np
 from diffusers import UNet2DModel, DDPMScheduler
 
 class Diffusion(nn.Module):
-  def __init__(self, image_size: int, in_channels: int, out_channels: int):
+  def __init__(self, image_size: int, in_channels: int, out_channels: int, timesteps: int):
     super().__init__()
     self.model = UNet2DModel(
       sample_size = image_size,
@@ -16,7 +16,23 @@ class Diffusion(nn.Module):
       layers_per_block = 2,
       block_out_channels = (64, 128, 256),
     )
-    self.noise_scheduler = DDPMScheduler(num_train_timesteps = 1000)
+    self.noise_scheduler = DDPMScheduler(num_train_timesteps = timesteps)
+    self.timesteps = timesteps
+    self.image_size = image_size
   def forward(self, noisy_image, timesteps):
     noise_pred = self.model(noisy_image, timesteps).sample # epsilon
     return noise_pred
+  @property
+  def timesteps(self):
+    return self.timesteps
+  def sample(self):
+    noise = torch.randn((1,3,self.image_size,self.image_size)).to(next(self.parameters()).device)
+    input = noise
+    for t in scheduler.timesteps:
+      with torch.no_grad():
+        noisy_residual = model(input, t).sample # epsilon(x_t, t)
+      previous_noisy_sample = scheduler.step(noisy_residual, t, input).prev_sample # x_{t-1}
+      input = previous_noisy_sample
+    image = (input / 2 + 0.5).clamp(0,1).squeeze()
+    image = torch.round(torch.permute(image, (1,2,0)) * 255).to(torch.uint8).cpu().numpy()[:,:,::-1]
+    return image
