d30abb4daa1056edfaec4dbc13da279d60556534
breadbread1984 <breadbread1984@163.com>
2024-10-18 13:43:03 +0800

    update

--- src/models/modules/diffusion.py
+++ src/models/modules/diffusion.py
@@ -14,19 +14,9 @@ class Diffusion(nn.Module):
       in_channels = in_channels,
       out_channels = out_channels,
       layers_per_block = 2,
-      block_out_channels = (128, 256, 256, 256),
-      down_block_types=(
-        "DownBlock2D",
-        "AttnDownBlock2D",
-        "DownBlock2D",
-        "DownBlock2D"
-      ),
-      up_block_types=(
-        "UpBlock2D",
-        "UpBlock2D",
-        "AttnUpBlock2D",
-        "UpBlock2D"
-      ),
+      block_out_channels = (64, 128, 256),
+      down_block_types=("DownBlock2D", "DownBlock2D", "AttnDownBlock2D"),
+      up_block_types=("AttnUpBlock2D", "UpBlock2D", "UpBlock2D"),
     )
     self.noise_scheduler = DDIMScheduler(num_train_timesteps = timesteps)
     self.noise_scheduler.set_timesteps(timesteps)
